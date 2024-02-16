66c1c593e76ea080724b732e1e065b4c60f93b8c
Mohiuddin Ahmed <mohiuddinahmed4047@gmail.com>
2024-02-17 00:37:52 +0600

    update life and score section another way using reusable function

--- script/index.js
+++ script/index.js
@@ -22,18 +22,29 @@ function handleKeyboardButtonPress(event){
     // check matched or not
     if(playerPressed === exactedAlphabet){
         console.log('you get a point');
-        console.log('you have pressed correctly', exactedAlphabet);
+        // console.log('you have pressed correctly', exactedAlphabet);
+
+        //----------------------------------------------------------------------
         // update score:
         // 1: get the current score 
-        const currentScoreElement = document.getElementById('current_score');
-        const currentScoreText = currentScoreElement.innerText;
-        const currentScore = parseInt(currentScoreText);
-        console.log(currentScore);
+        // const currentScoreElement = document.getElementById('current_score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+        
         // 2: increase the score by 1
-        const newScore = currentScore + 1;
+        // const newScore = currentScore + 1;
+        
         // 3: show the update score
-        currentScoreElement.innerText = newScore
-
+        // currentScoreElement.innerText = newScore
+        //----------------------------------------------------------------------
+        
+        // another way
+        const currentScore = getTextElementValueByID('current_score');
+        console.log(currentScore);
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current_score', updatedScore);
+        
         // start a new round 
         const element = document.getElementById(exactedAlphabet);
         element.classList.remove('bg-orange-400');
@@ -42,15 +53,21 @@ function handleKeyboardButtonPress(event){
     else{
         console.log('you missed,you lost a life.');
         // step-1: get the current life number.
-        const currentLifeElement = document.getElementById('current_life');
-        const currentLifeText = currentLifeElement.innerText;
-        const currentLife = parseInt(currentLifeText);
-
+        // const currentLifeElement = document.getElementById('current_life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        
         // step-2: reduce the life count 
-        const newLife = currentLife - 1;
-
+        // const newLife = currentLife - 1;
+        
         // step-3: display the updated life count 
-        currentLifeElement.innerText = newLife;
+        // currentLifeElement.innerText = newLife;
+        
+        //----------------------------------------------------------------------
+        const currentLife = getTextElementValueByID('current_life');
+        const updateLife = currentLife - 1;
+        setTextElementValueById('current_life', updateLife);
+
     }
 }
 // capture keyboard key press
@@ -58,7 +75,7 @@ document.addEventListener('keyup', handleKeyboardButtonPress)
 function continueGame(){
     // step-1: generate a random alphabet
     const alphabet = getRandomAlphabet();
-    console.log('your random alphabet: ', alphabet);
+    // console.log('your random alphabet: ', alphabet);
 
     // stet-2: set randomly generated alphabet to the screen (show it).
     const currentAlphabetElement = document.getElementById('current_alphabet');
--- script/utility.js
+++ script/utility.js
@@ -13,6 +13,18 @@ function setBackgroundColorById(elementId){
     element.classList.add('bg-orange-400')
 }
 
+function getTextElementValueByID(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
 function getRandomAlphabet() {
     // create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
